c5f6dcc54d605a82ca731ceca5e68b5addfef1f2
Brian Pennington <srsrec@gmail.com>
2018-10-16 15:16:27 -0500

    updated score funtions

--- dice.js
+++ dice.js
@@ -1,4 +1,4 @@
-// determie and state game rules (alert)
+
     
 // the object of the game is to get as high of score possible, but the final score MUST be even.
 // player gets 10 rolls total: 5 determined by a pre determined 6 sided die, the last 5 rolls given a choice of 4, 6, 8, 10, 13, 19 sided dice
@@ -8,12 +8,15 @@ let playerName = prompt("Before beginning, please enter your name");
 let startingScore = 0; 
 let gameCounter = 0;
 let gameTotalThusFar;
+let gameScoreCard = [];
+
 
 function toProceed() { 
     if (gameCounter != 0) { //if they play they will have a count other than zero.
         let doYouWantToPlayAgain = prompt("You know the drill.. do you wanna play again? Y/N");
         if (doYouWantToPlayAgain == "N" || doYouWantToPlayAgain == "n") {
             alert("Yeah I don't blame ya...");
+            // final score function
         }
         else {
             alert("Ok you sadist.. (press OK)");
@@ -21,7 +24,7 @@ function toProceed() {
         }
     } 
     else {
-        let doYouWantToPlay = prompt("The point of the game is to get as high of an EVEN NUMBER score as possible. The first 5 rolls are automatic (6 sided die), and the last 5 are optional (4, 6, 8, 11, 13, or 19) to give you many scoring opportunities. But remember: the higher value dice give you an increased chance at an ODD overall score. Would you like to play? Y/N");
+        let doYouWantToPlay = prompt("Welcome to DEATH CARD! The goal is to accumulate as high of a dice score as possible, as long if it's even. If you end up with an odd total, then you must face the DEATH CARD round, and that's no fun. There are odd-sided dice in the second round, increasing your chance of odd number roll tallys (can be good or bad depending where you're at.) Would you like to play? Y/N");
         if (doYouWantToPlay == "N" || doYouWantToPlay == "n") {
         alert("Well alright then..");
     } 
@@ -34,7 +37,7 @@ function toProceed() {
 console.log(toProceed());
 
 function briansLousyDiceGame(playerName) {
-    alert("Hello " + playerName + ", hit OK to generate your first five rolls of the 6 die.");
+    alert("Ok " + playerName + ", hit OK to generate your first five rolls of the 6 die.");
     //let gameTotalThusFar
     let diceSum;
 
@@ -50,7 +53,7 @@ function briansLousyDiceGame(playerName) {
         let diceValfive = sixSidedDiceRoll();
         console.log(diceValfive);
         diceSum = startingScore + diceValone + diceValtwo + diceValthree + diceValfour + diceValfive;
-        alert("Your current score is " + diceSum + " (check the console to view each roll), press OK to choose your next dice roll");
+        alert("Your first 5 dice rolls added up to " + diceSum + " (check the console to view each roll), press OK to choose your next dice roll");
     }   console.log(fiveRandomRolls(startingScore));
     
     function multiDiceCodeSolution(diceSum) { // more sophisticated code
@@ -87,26 +90,35 @@ function briansLousyDiceGame(playerName) {
             let arrayTotal = rollTallied.reduce(reducer);
             gameTotalThusFar = arrayTotal + diceSum;
             alert("Your total for this spin was " + computerDiceRoll + ", giving you a total of " + gameTotalThusFar + "!");
-            } // for loop, 5 player turns
             gameCounter++;
+        } // for loop, 5 player turns
+            
             
         }   console.log(multiDiceCodeSolution(diceSum));
             
-        
-        
+            let scoreCard = playerName + ": " + gameTotalThusFar + ". ";
+            gameScoreCard.push(scoreCard); 
+
             if (gameTotalThusFar % 2 === 1) {
-                prompt("Uh oh, that's an odd score. However, you still have a chance! Choose a card between 1 and 5. You have a 80/20 chance of picking a LIFE CARD, which will let you play again! If you get a DEATH CARD, your game is still over. Soooo.. pick a card: 1, 2, 3, 4, or 5.");
+                prompt("Uh oh, that's an odd score. However, you still have a chance! Choose a card between 1 and 3. You have a 66% chance of picking the DEATH CARD, which will end your streak. Soooo.. pick a card: 1, 2, or 3.");
                 let meaninglessCardPick = Math.random(); // yes, the "card pick" is completely pointless
-                    if (meaninglessCardPick >= .5 && meaninglessCardPick <= .7) { // the vicinity of .666
+                    if (meaninglessCardPick >= .33 && meaninglessCardPick <= 1) { 
                         alert("Oh snap, you got the DEATH CARD. Game over.");
+                        // final score function
                     }
                     else {
                         alert("Phew, you were saved by the LIFE CARD!");
+                        if (gameCounter > 7) {
+                            alert("Here are your accumulated scores: " + gameScoreCard);
+                        }
                         console.log(toProceed());
                     }
             }
             else {
                 alert("Cool, you got an EVEN number score!");
+                if (gameCounter > 7) {
+                    alert("Here are your accumulated scores: " + gameScoreCard);
+                }
                 console.log(toProceed());
             }
 
@@ -115,7 +127,7 @@ function briansLousyDiceGame(playerName) {
 
 
 
-function sixSidedDiceRoll()  { //primitive, first attempt solution. I left it to document my progress.
+function sixSidedDiceRoll()  { //primitive, first attempt die solution. I left it to document my progress.
     var dice = Math.random();
     if (dice <= .17) {
         dice = 1;   
